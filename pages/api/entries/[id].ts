75b43f584b9aadb1bc0a6c6a9e239e1a0d3e0a4f
victor toledo <querynetworking@gmail.com>
2023-03-29 23:43:15 -0300

    creacion de endpoint de obtejer entrada por id

--- pages/api/entries/[id].ts
+++ pages/api/entries/[id].ts
@@ -18,12 +18,31 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
 
     switch ( req.method ) {
         case 'PUT':
-            return updateEntry( req, res);
+            return updateEntry(req, res);
+
+        case 'GET':
+            return getEntry(req, res);
     
         default:
-            return res.status(400).json({ message: 'Método no existe'});
+            return res.status(400).json({ message: 'Método no existe ' + req.method });
     }
 
+}
+
+const getEntry = async( req: NextApiRequest, res: NextApiResponse ) => {
+
+    const { id } = req.query;
+
+    await db.connect();
+    const entryInDB = await Entry.findById( id );
+    await db.disconnect();
+
+    if ( !entryInDB ) {
+        return res.status(400).json({ message: 'No hay entrada con ese ID: ' + id })
+    }
+
+    return res.status(200).json( entryInDB );
+    
 }
 
 const updateEntry = async( req: NextApiRequest, res: NextApiResponse<Data> ) => {
@@ -55,5 +74,5 @@ const updateEntry = async( req: NextApiRequest, res: NextApiResponse<Data> ) => 
         
     }
 
-
 }
+
